04cb39209e6ae92910e17a3e43ac410262378983
Shawnsajivarughese <150415378+Shawnsajivarughese@users.noreply.github.com>
2024-11-01 12:04:03 +0530

    print1.java

---
+++ print1.java
@@ -0,0 +1,8 @@
+public class calss{
+    public static void main (String[] args){
+        String x = "Cool";
+        String y = "Water";
+        System.out.println(y + x);
+        System.out.println(x);
+    }
+}
