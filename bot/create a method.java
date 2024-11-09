8db40309cf9a2f61e9a354b69be3e991914b38b9
Shawnsajivarughese <150415378+Shawnsajivarughese@users.noreply.github.com>
2024-11-09 11:08:10 +0530

    create a method.java

---
+++ bot/create a method.java
@@ -0,0 +1,10 @@
+public class Method{
+    static void myMethod(){
+        System.out.println("java is powerfull");
+    }
+    public static void main(String[] args){
+        myMethod();
+        myMethod();
+        myMethod();
+    }
+}
